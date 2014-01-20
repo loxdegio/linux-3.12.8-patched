cacd10187f4d8d5b0be5be065e57e2141397bcaf
loxdegio <loxdegio@hotmail.com>
2014-01-20 15:56:01 +0100

    Primo commit - Leggere il README.md per ulteriori dettagli

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ linux-3.12.8-patched
 ====================
 
 Versione del geek kernel con le patch scelte da me
+
+bfq ck gentoo red-hat
---
+++ drivers/media/common/saa716x/saa716x_budget.h
@@ -0,0 +1,17 @@
+#ifndef __SAA716x_BUDGET_H
+#define __SAA716x_BUDGET_H
+
+#define TWINHAN_TECHNOLOGIES	0x1822
+#define TWINHAN_VP_3071		0x0039
+#define TWINHAN_VP_1028		0x0044
+#define TWINHAN_VP_6002		0x0047
+
+#define KNC_One			0x1894
+#define KNC_Dual_S2		0x0110
+
+#define TECHNISAT		0x1AE4
+#define SKYSTAR2_EXPRESS_HD	0x0700
+#define TEVII			0x9022
+#define TEVII_S472		0xd472
+
+#endif /* __SAA716x_BUDGET_H */
