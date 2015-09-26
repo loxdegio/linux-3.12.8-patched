73a2109da9ad804e36954100bab07a7609a347d9
loxdegio <loxdegio@hotmail.com>
2015-09-26 18:07:27 +0200

    Upgraded v4.2.1-zenx

--- mm/memory.c
+++ mm/memory.c
@@ -2066,7 +2066,7 @@ static inline int wp_page_reuse(struct mm_struct *mm,
 		}
 
 		if (!page_mkwrite)
-			file_update_time(vma->vm_file);
+			vma_file_update_time(vma);
 	}
 
 	return VM_FAULT_WRITE;
@@ -2132,11 +2132,9 @@ static int wp_page_copy(struct mm_struct *mm, struct vm_area_struct *vma,
 				dec_mm_counter_fast(mm, MM_FILEPAGES);
 				inc_mm_counter_fast(mm, MM_ANONPAGES);
 			}
-		uksm_bugon_zeropage(orig_pte);
+			uksm_bugon_zeropage(orig_pte);
 		} else {
 			uksm_unmap_zero_page(orig_pte);
-			inc_mm_counter_fast(mm, MM_ANONPAGES);
-		}
 		flush_cache_page(vma, address, pte_pfn(orig_pte));
 		entry = mk_pte(new_page, vma->vm_page_prot);
 		entry = maybe_mkwrite(pte_mkdirty(entry), vma);
